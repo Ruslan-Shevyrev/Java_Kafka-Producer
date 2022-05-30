918e60ab9c800e77afd79d7ca83e7d88b9e4ef48
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2022-05-30 19:36:44 +0300

    add producer

--- src/main/java/com/ruslanshevyrev/kafka/java/KafkaSentProducer.java
+++ src/main/java/com/ruslanshevyrev/kafka/java/KafkaSentProducer.java
@@ -13,43 +13,28 @@ import org.apache.kafka.common.serialization.StringSerializer;
 public class KafkaSentProducer {
 
 	public static void main(String[] args) {
-		Properties props = ProducerConfiguration
-				.createProducer("10.191.209.208:9092,10.191.209.163:9092,10.191.209.157:9092", "clientid1");
+		//Properties props = ProducerConfiguration
+		//		.createProducer("10.191.209.208:9092,10.191.209.163:9092,10.191.209.157:9092", "clientid1");
 
-		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		//props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-		Producer<Long, String> producer = new KafkaProducer<Long, String>(props);
+		//Producer<Long, String> producer = new KafkaProducer<Long, String>(props);
 		
-		for (int index = 0; index < KafkaConstants.MESSAGE_COUNT; index++) {
-			String event = "This is record " + index;
-			String topicName = "demo-session4";
-			ProducerRecord<Long, String> record = new ProducerRecord(topicName, event);
-			//ProducerRecord<Long, String> record2 = new ProducerRecord("xyz-session4", event);
-			
-			try {
-				Thread.sleep(1000);
-				// System.out.println("called");
-				RecordMetadata metadata = producer.send(record).get();
-				
-				//RecordMetadata metadata2 = producer.send(record2).get();
-				
-				/*
-				 * //producer.send(record2).get(); System.out.println("Record sent with key " +
-				 * index + " to partition " + metadata.partition() + " with offset " +
-				 * metadata.offset()); //producer.send(record2).get();
-				 * System.out.println("Record sent with key " + index + " to partition " +
-				 * metadata2.partition() + " with offset " + metadata2.offset());
-				 */
-			} catch (ExecutionException e) {
-				System.out.println("Error in sending record");
-				System.out.println(e);
-			} catch (InterruptedException e) {
-				System.out.println("Error in sending record");
-				System.out.println(e);
-			} catch (Exception e) {
-				System.out.println("Error in sending record 2");
-				System.out.println(e);
-			}
-		}
+                Properties kafkaProps = new Properties();
+
+                kafkaProps.put("bootstrap.servers", "localhost:9092");
+                kafkaProps.put("key.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+                kafkaProps.put("value.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+                KafkaProducer producer = new KafkaProducer<String, String>(kafkaProps);
+
+                ProducerRecord<String, String> record =
+                new ProducerRecord<>("kafkaTest", "From Test2");
+                try {
+                    producer.send(record).get();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
 	}
 }
