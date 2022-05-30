6ed87cf00cfdce199a4a73142105e8fabfafc2c8
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2022-05-30 21:36:01 +0300

    KafkaReadConsumer.java

--- src/main/java/com/ruslanshevyrev/kafka/java/KafkaSentProducer.java
+++ src/main/java/com/ruslanshevyrev/kafka/java/KafkaSentProducer.java
@@ -30,7 +30,7 @@ public class KafkaSentProducer {
                 KafkaProducer producer = new KafkaProducer<String, String>(kafkaProps);
 
                 ProducerRecord<String, String> record =
-                new ProducerRecord<>("kafkaTest", "From Test");
+                new ProducerRecord<>("kafkaTest", "test","From Test1");
                 try {
                     producer.send(record).get();
                 } catch (Exception e) {
